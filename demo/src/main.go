3edb1c68340865d21d46b25a34d6e9027cdb9715
Vitalii Umanets <umanetsvitaliy@gmail.com>
2023-11-03 17:22:47 +0200

    demo

--- demo/src/main.go
+++ demo/src/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-
+	fmt.Println("Hello World!")
 	http.ListenAndServe(":8080", http.FileServer(http.Dir("")))
 }
